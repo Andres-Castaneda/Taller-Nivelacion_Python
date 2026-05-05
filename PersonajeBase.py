dc1b2511a24cc1e65976626fce2fdf5e7d6eb2bd
Andres Castañeda <afcastanedac@udistrital.edu.co>
2026-05-05 06:50:46 -0500

    [nivelacion-n1] bloque1 - tipos de datos personaje rpg Andres Castañeda y Quedin Vanegas

---
+++ PersonajeBase.py
@@ -0,0 +1,46 @@
+# Variables del personaje RPG 
+nombre = 'Gandalf' 
+nivel = 5
+vida = 80 
+vida_maxima = 100
+esta_vivo = True 
+clase = 'Mago' 
+puntos_ataque = 15 
+puntos_defensa = 10
+Mana = 120
+ 
+# Verificar tipos 
+
+print(type(nombre))   # <class 'str'> 
+
+print(type(nivel))    # <class 'int'> 
+
+print(type(vida))     # <class 'float'> 
+
+print(type(esta_vivo)) # <class 'bool'> 
+
+ 
+
+print(f'{nombre}[{clase}] Nivel: {nivel} | Vida: {vida}| Mana: {Mana}') 
+
+ataque = 15 
+
+dano = float(ataque) * 1.5  # cast 
+
+msg = 'Dano: ' + str(dano)  # explicito 
+
+# Leer del usuario (input) 
+
+nom = input('Nombre: ') 
+
+niv = int(input('Nivel: ')) 
+
+print(f'{nom} Nv.{niv}') 
+
+ 
+
+# f-strings (muy utiles) 
+
+vida = 87.5 
+
+print(f'Vida: {vida:.1f}%') 
