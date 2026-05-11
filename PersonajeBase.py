1f3019c013a77ee0d47c97f265363f522f352986
Quedin Vanegas <qavanegasr@udistrital.edu.co>
2026-05-11 18:08:49 -0500

    [nivelacion-n1] bloque1 - tipos de datos personaje rpg

---
+++ PersonajeBase.py
@@ -0,0 +1,21 @@
+
+nombre = 'Aragorn'
+nivel = 1
+vida = 100.0
+vida_maxima = 100.0
+esta_vivo = True
+clase = 'Guerrero'
+puntos_ataque = 15
+puntos_defensa = 10
+
+nombre_mago = 'Gandalf'
+nivel_mago = 5
+vida_mago = 80.0
+clase_mago = 'Mago'
+mana_mago = 120
+
+print(f'{nombre_mago} [{clase_mago}] Nv.{nivel_mago} | Vida: {vida_mago} | Mana: {mana_mago}')
+
+print(type(nombre))   
+print(type(nivel))    
+print(type(vida))    
